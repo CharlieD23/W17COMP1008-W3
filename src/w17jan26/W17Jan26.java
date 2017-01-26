0833a9c4af3f4c811cf4ce087ec9aa9db83f2c67
Charlie <Charlie@10.250.14.139>
2017-01-26 13:49:31 -0500

    Final commit for the day

--- src/w17jan26/W17Jan26.java
+++ src/w17jan26/W17Jan26.java
@@ -27,6 +27,15 @@ public class W17Jan26 {
         emp1.recordHoursWorked(8);
 
         System.out.printf("%s%n", emp1.getPayCheck());
+        
+        CommisionEmployee emp2 = new CommisionEmployee("Joe","Bo",1235,LocalDate.of(1987, Month.MAY, 6), 5);
+        
+        System.out.println("The second employee is "+emp2.toString());
+        
+        emp2.recordSales(3234.33);
+        emp2.recordSales(983.33);
+
+        System.out.printf("%s%n", emp2.getPayCheck());
     }
     
 }
